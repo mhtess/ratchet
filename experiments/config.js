9b65f4e0ce5e3e13c3b0cc5f84adde26a8fd6393
mhtess <mtessler@stanford.edu>
2016-12-19 14:04:20 -0800

    add basic elements for server expt

---
+++ experiments/config.js
@@ -0,0 +1,12 @@
+var configs = {
+	"play_time": 60,
+	"use_db": true,
+	"http_port": 8080,
+	"https_port": 4343,
+	"private_key": "sslcert/server.key",
+	"certificate": "sslcert/server.crt",
+	"mongo_host": "mongo_host",
+	"mongo_user": "mongo_user",
+	"mongo_password": "mongo_password",
+	"mongo_database": "mongo_database",
+}
---
+++ experiments/server.js
@@ -0,0 +1,162 @@
+var express = require('express'),
+ 		app = express(),
+ 		fs = require('fs'),
+ 		vm = require('vm'),
+ 		moment = require('moment');
+
+vm.runInThisContext(fs.readFileSync(__dirname + '/config.js'))
+var use_db = configs.use_db
+var time_to_play = configs.play_time
+var exit_survey_url = configs.exit_survey_url
+
+var getTimestamp = function(){
+	var date = moment().format().slice(0, 10)
+	var time = moment().format().slice(11, 19)
+	return date + ' ' + time
+}
+
+try {
+	var
+		 https = require('https'),
+		 port = configs.https_port,
+		 privateKey = fs.readFileSync(configs.private_key),
+		 certificate = fs.readFileSync(configs.certificate),
+		 credentials = {key: privateKey, cert: certificate},
+		 server = https.createServer(credentials, app),
+		 io = require('socket.io').listen(server);
+} catch(err){
+	console.log("HTTPS failed to launch -- falling back to HTTP")
+	var
+		 http = require('http'),
+		 port = configs.http_port,
+		 server = http.createServer(app),
+		 io = require('socket.io').listen(server)
+}
+
+if (use_db) {
+	var mongodb = require('mongodb');
+	//We need to work with "MongoClient" interface in order to connect to a mongodb server.
+	var MongoClient = mongodb.MongoClient;
+	// var url = 'mongodb://superAdmin:admin123@localhost:27017/admin';
+	var url = 'mongodb://superAdmin:admin123@localhost:27017/mydb?';
+	// var url = 'mongodb://[username:password@]host1[:port1][/[database][?options]]'
+	var db;
+	// Initialize connection once
+	MongoClient.connect(url, function(err, database) {
+	  // if(err) throw err;
+		if (err) {
+	    console.log('Unable to connect to the mongoDB server. Error:', err);
+	  } else {
+			console.log('Connection established to', url);
+		  db = database;
+		  // Start the application after the database connection is ready
+		  app.listen(3000);
+		  console.log("Listening on port 3000");
+		}
+	});
+
+	// var database = require(__dirname + '/js/database')
+	// var connection = mysql.createConnection({
+	// 	host		: configs.mysql_host,
+	// 	user		: configs.mysql_user,
+	// 	password	: configs.mysql_password,
+	// 	database	: configs.mysql_password
+	// })
+}
+
+app.use(express.static(__dirname));
+
+app.get(/^(.+)$/, function(req, res){
+     console.log('static file request : ' + req.params);
+     console.log("ACCESS: " + req.params[0])
+     res.sendFile(__dirname + req.params[0])
+ });
+
+//namespace for assigning experiment parameters
+var expnsp = io.of('/experiment-nsp')
+// this is run when the client is detected
+expnsp.on('connection', function(socket){
+
+	var assignCondition = function(){
+		//code for determining the condition of the experiment
+		var condition = 'a' //
+		// send the client "condition var"
+		socket.emit('condition', condition)
+	}
+
+	assignCondition()
+})
+
+var gamensp = io.of('/game-nsp')
+gamensp.on('connection', function(socket){
+
+	var hs = socket.handshake
+	var query = require('url').parse(socket.handshake.headers.referer, true).query
+	var condition = (query.condition) ? query.condition : 'a' //try to pull condition from url, if fail --> default to 'a'
+	var user = (query.workerId) ? query.workerId : 'undefinedID'
+
+	var inventory = {
+		pocket: 'empty',
+		apples: 0,
+		fishes: 0
+	}
+
+	console.log("Connection from user: " + user + ".")
+
+	if (use_db) {
+		database.addPlayer(user, condition)
+	}
+
+	//function for updating database
+	var updateDB = function(action){
+		if(use_db){
+			database.updatePlayer(user, condition, action, inventory.pocket, inventory.apples, inventory.fishes)
+		}
+	}
+
+	//counts down until time_to_play has run out
+	var timer = function(seconds){
+		setTimeout(function(){
+			if (seconds >= 1){
+				timer(seconds - 1)
+			}else{
+				var destination = '/exitsurvey.html'
+				socket.emit('redirect', destination)
+				console.log("redirecting " + user)
+			}
+		}, 1000)
+	}
+
+	timer(time_to_play)
+
+	socket.on('action', function(action){
+		if(action == 'get apple'){
+			inventory.apples += 1
+		}else if(action == 'shoot apple'){
+			inventory.apples -= 1
+		}else if(action == 'get rock'){
+			inventory.pocket = 'rock'
+		}else if(action == 'shoot rock'){
+			inventory.pocket = 'empty'
+		}
+
+		if(use_db){
+			updateDB(action)
+		}
+	});
+
+	var exportCSV = function(){
+		//function for exporting CSVs goes here
+	}
+
+	socket.on('discconect', function(){
+		exportCSV()
+	})
+})
+
+server.listen(port, function(){
+	console.log("Game server listening port " + port + ".")
+	if(use_db){
+		console.log("Logging results in mysql database.")
+	}
+})
