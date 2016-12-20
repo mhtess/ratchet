65a901eedda3228eea24115e8c6cf59b037d00d3
mhtess <mtessler@stanford.edu>
2016-12-19 18:16:33 -0800

    chain/gen assignment logic

--- experiments/server.js
+++ experiments/server.js
@@ -9,6 +9,10 @@ var use_db = configs.use_db
 var time_to_play = configs.play_time
 var exit_survey_url = configs.exit_survey_url
 
+var nChains = config.nChains; // number of chains
+var nGenPerChain = config.nGenPerChain; // number of gens in a chain
+var bigN = nChains * nGenPerChain;
+
 var getTimestamp = function(){
 	var date = moment().format().slice(0, 10)
 	var time = moment().format().slice(11, 19)
@@ -33,6 +37,19 @@ try {
 		 io = require('socket.io').listen(server)
 }
 
+// chains collection contains documents that look like:
+// {gen: 4, chain: 2, genInProgress: false, chainInProgress: true, workerid: workerid, condition: condition, startTime: time}
+
+// messages collection
+// might want to make different collections for different conditions (e.g., language vs. data passing)
+// {gen: 4, chain: 2, message: "as the bugs get bigger, the trees get smaller", workerid: workerid, condition: "language"}
+// {gen: 4, chain: 2, message: [{stimulus: 0.34, response: 0.76}, ... ], workerid: workerid, condition: "data_incidental"}
+
+// trials collection (for discrete time experiments)
+// consult nathaniel's mysql code for ideas for continuous time expts
+// {gen: 4, chain: 2, trial: 14, stimulus: 0.34, response: 0.76, feedback: false, workerid: workerid, condition: condition}
+
+
 if (use_db) {
 	var mongodb = require('mongodb');
 	//We need to work with "MongoClient" interface in order to connect to a mongodb server.
@@ -40,7 +57,7 @@ if (use_db) {
 	// var url = 'mongodb://superAdmin:admin123@localhost:27017/admin';
 	var url = 'mongodb://superAdmin:admin123@localhost:27017/mydb?';
 	// var url = 'mongodb://[username:password@]host1[:port1][/[database][?options]]'
-	var db;
+	var db, chain_collection, results_collection;
 	// Initialize connection once
 	MongoClient.connect(url, function(err, database) {
 	  // if(err) throw err;
@@ -49,6 +66,9 @@ if (use_db) {
 	  } else {
 			console.log('Connection established to', url);
 		  db = database;
+			chain_collection = db.collection('chains');
+			messages_collection = db.collection('messages');
+			trials_collection = db.collection('trials');
 		  // Start the application after the database connection is ready
 		  app.listen(3000);
 		  console.log("Listening on port 3000");
@@ -77,14 +97,89 @@ var expnsp = io.of('/experiment-nsp')
 // this is run when the client is detected
 expnsp.on('connection', function(socket){
 
+
+	// for max chain val
+	var newestChain = chain_collection.find().sort({chain:-1}).limit(1)
+	var nextChain = newestChain.chain + 1;
+	// have we run out of chains?
+	var outOfChains = newestChain == nChains;
+
+	// or
+	// var newestChain = chain_collection.aggregate(
+	//    [{$group: { _id: "$chain", maxChain: { $max: "$chain" } }}]
+	//  );
+
+	// of the chains that are NOT finished (chainInProgress: true) and
+	// don't currently have a gen running (genInProgress: false)
+	// find the maximum generation
+	var nextGens = chain_collection.aggregate(
+   [
+		 {
+			 $match:
+				 {'genInProgress': false},
+				 {'chainInProgress': true}
+		 },
+     {$group: { _id: "$chain", maxGen: { $max: "$gen" } }}
+   ]
+ );
+
+ 	var participantMustWait = (!nextGens && outOfChains);
+
+	// the generation that's been running the longest
+	// only run under certain conditions
+	var longestRunningGen = chain_collection.aggregate(
+		 [
+			 {$match: {'chainInProgress': true}, {'genInProgress': true} },
+			 {$group: { _id: "$chain", time: { $max: "$startTime" } }}
+		 ]
+	 );
+
+	 // CHAIN ASSINGNMENT LOGIC
+	// if nextGens exists, we assign the incoming participant to the chain closest to completion
+	// could imagine other schemes (e.g., the chain furthest from completion);
+
+	// if nextGens DOESNT exist (if there are no chains in progress that have a ready slot):
+	// then make a new chain (if we can)
+	// otherwise, assign the participant to the chain that's been running the longest
+	var lastGenInChain = nextGens ? _.max(nextGens, "gen") :
+										outOfChains ? longestRunningGen :
+										-1;
+
+
+	var chain = nextGens ? lastGenInChain.chain :
+						outOfChains ? longestRunningGen.chain :
+						nextChain;
+
+	var condition = nextGens ? lastGenInChain.condition :
+						outOfChains ? longestRunningGen.chain :
+						assignCondition();
+
+	// is this the last generation in the chain
+	var chainInProgress = lastGenInChain.gen + 1 < nGenPerChain;
+
+	chain_collection.insert({
+		gen: lastGenInChain.gen + 1, // increment gen counter
+		chain: chain, // same chain as lastGenInChain
+		genInProgress: true, // so that the next participant doesn't get assigned to this same position
+		chainInProgress: chainInProgress // is this the end of the chain
+	})
+
+	var dataToPass = messages_collection.find({gen: lastGenInChain.gen, chain: chain}).message
+
+	// TO DO
+	// proper handling of start time
+	// figure out waiting situation,
+	// icing: estimate wait time for next participant and display
+
 	var assignCondition = function(){
 		//code for determining the condition of the experiment
-		var condition = 'a' //
+		// TO DO: counterbalance
+		var condition = _.sample(['language', 'data_incidental']) //
 		// send the client "condition var"
-		socket.emit('condition', condition)
 	}
 
-	assignCondition()
+	socket.emit('dataToPass', dataToPass);
+
 })
 
 var gamensp = io.of('/game-nsp')
@@ -105,6 +200,7 @@ gamensp.on('connection', function(socket){
 
 	if (use_db) {
 		database.addPlayer(user, condition)
+
 	}
 
 	//function for updating database
@@ -113,6 +209,17 @@ gamensp.on('connection', function(socket){
 			database.updatePlayer(user, condition, action, inventory.pocket, inventory.apples, inventory.fishes)
 		}
 	}
+
+	collection.insert([user1, user2, user3], function (err, result) {
+		 if (err) {
+			 console.log(err);
+		 } else {
+			 console.log('Inserted %d documents into the "users" collection. The documents inserted with "_id" are:', result.length, result);
+		 }
+		 //Close connection
+		 db.close();
+	 })
+
 
 	//counts down until time_to_play has run out
 	var timer = function(seconds){
@@ -157,6 +264,6 @@ gamensp.on('connection', function(socket){
 server.listen(port, function(){
 	console.log("Game server listening port " + port + ".")
 	if(use_db){
-		console.log("Logging results in mysql database.")
+		console.log("Logging results in mongo database.")
 	}
 })
