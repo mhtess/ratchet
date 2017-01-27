82c6b07f2417c2fcd087a102c7823f9a10d42ab3
Nathaniel Knopf <nathanielknopf@gmail.com>
2017-01-27 13:40:26 -0500

    update config.js

--- experiments/config.js
+++ experiments/config.js
@@ -1,4 +1,5 @@
 var configs = {
+	"nChains": 8, //half of chains go to language condition, half to data incidental condition
 	"http_port": 80,
 	"https_port": 443,
 	"private_key": "sslcert/server.key",
